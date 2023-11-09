9715cef18d4ad6789f2746c606d2cd538d49ee28
qutluq <kootlook.applications@gmail.com>
2023-11-09 22:59:58 +0600

    refactor: move news feed from home to component

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,40 +1,9 @@
-'use client'
-
-import { useEffect, useState } from 'react'
-
-import { getAllUsers, getUsersPosts } from '@/backend/api-client'
-
-import type { News, User } from '@/types'
+import { NewsFeed } from '@/components/news'
 
 export const Home = () => {
-  const [users, setUsers] = useState<User[]>([])
-  const [feed, setFeed] = useState<News[]>([])
-
-  useEffect(() => {
-    getAllUsers()
-      .then((response) => {
-        if (!response) return
-        setUsers(() => response)
-      })
-      .catch((error) => {
-        console.error(`Users fetch failed: ${error}`)
-      })
-
-    getUsersPosts(1)
-      .then((response) => {
-        if (!response) return
-        setFeed(() => response)
-      })
-      .catch((error) => {
-        console.error(`User's news feed fetch failed: ${error}`)
-      })
-  }, [])
-
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center text-lg font-bold">
-      {feed.map((news) => (
-        <div key={news.id}>{news.title}</div>
-      ))}
+    <div className="flex w-full flex-row justify-center">
+      <NewsFeed />
     </div>
   )
 }
---
+++ src/components/news/news-feed.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import { getUsersPosts } from '@/backend/api-client'
+
+import type { News } from '@/types'
+
+const newsFeedEmpty = [
+  {
+    id: 1,
+    author_id: 1,
+    body: 'No news available',
+    title: 'Something went wrong',
+    date: new Date(),
+    created_at: new Date(),
+  },
+] as News[]
+
+export const NewsFeed = () => {
+  // const [users, setUsers] = useState<User[]>([])
+  const [feed, setFeed] = useState<News[] | undefined>()
+
+  useEffect(() => {
+    // getAllUsers()
+    //   .then((response) => {
+    //     if (!response) return
+    //     setUsers(() => response)
+    //   })
+    //   .catch((error) => {
+    //     console.error(`Users fetch failed: ${error}`)
+    //   })
+
+    getUsersPosts(1)
+      .then((response) => {
+        if (!response) {
+          setFeed(newsFeedEmpty)
+          return
+        }
+        setFeed(() => response)
+      })
+      .catch((error) => {
+        console.error(`User's news feed fetch failed: ${error}`)
+      })
+  }, [])
+
+  return (
+    <div className="flex h-96 w-[500px] flex-col items-center justify-center bg-white text-lg font-bold">
+      {!feed
+        ? 'Loading...'
+        : feed.map((news) => <div key={news.id}>{news.title}</div>)}
+    </div>
+  )
+}
