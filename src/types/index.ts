10e205dda84a306eec6d49578c0a6fe3a2fb7664
qutluq <kootlook.applications@gmail.com>
2023-11-09 21:17:17 +0600

    feat: get user's news feed from db

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,20 @@
 export type User = {
   id: number
-  created_at: Date
-  username: string
-  email: string
   about: string
-  image: string
+  created_at: Date
+  email: string
   firstname: string
+  image: string
   lastname: string
   password: string
+  username: string
+}
+
+export type News = {
+  id: number
+  author_id: number
+  body: string
+  created_at: Date
+  date: Date
+  title: string
 }
