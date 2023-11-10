08d56c4caf8a1290a48f28532109dd001866a9b9
qutluq <kootlook.applications@gmail.com>
2023-11-10 17:36:58 +0600

    feat: add sign-up api

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server'
+
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+
+import type { Database } from '@/types/database.types'
+import type { NextRequest } from 'next/server'
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient<Database>({ req, res })
+  await supabase.auth.getSession()
+  return res
+}
